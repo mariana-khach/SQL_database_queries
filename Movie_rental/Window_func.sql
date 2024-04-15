800d10623d883a7959aedfb24b3c579e40ac5181
mariana-khach <64562813+mariana-khach@users.noreply.github.com>
2024-04-15 09:32:20 -0400

    First commit

--- Movie_rental/Window_func.sql
+++ Movie_rental/Window_func.sql
@@ -3,4 +3,19 @@ SELECT customer.first_name,customer.last_name, rental_date FROM customer
 JOIN rental ON rental.customer_id=customer.customer_id
 )
 SELECT first_name,last_name,
-	rental_date-lag(rental_date) OVER(ORDER BY rental_date) FROM custrent_cte;
+	rental_date-lag(rental_date) OVER(ORDER BY rental_date) AS time_diff FROM custrent_cte;
+
+
+SELECT * FROM(
+WITH rentpay_cte AS(
+SELECT title, rating,rental_date FROM customer
+JOIN rental ON rental.customer_id=customer.customer_id
+JOIN inventory ON rental.inventory_id=inventory.inventory_id
+JOIN film ON film.film_id=inventory.film_id
+)
+SELECT title,
+	ROW_NUMBER() OVER(PARTITION BY rating ORDER BY rental_date) AS rownum
+	FROM rentpay_cte
+		WHERE (rating='PG' OR rating='PG-13') 
+) AS t
+WHERE t.rownum=1 ;
